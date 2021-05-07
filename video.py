ac6f413ae678ac9c58f5974fdefedb384a7e1676
just_benedict_it <shsms9223@gmail.com>
2021-05-07 11:00:08 +0900

    tello_test_vid

---
+++ video.py
@@ -0,0 +1,33 @@
+import time, cv2
+from threading import Thread
+from djitellopy import Tello
+
+tello = Tello()
+
+tello.connect()
+
+keepRecording = True
+tello.streamon()
+frame_read = tello.get_frame_read()
+
+def videoRecorder():
+    height, width, _ = frame_read.frame.shape
+    video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'), (width, height))
+
+    while keepRecording:
+        video.write(frame_read.frame)
+        time.sleep(1/30)
+
+    video.release()
+
+recorder = Thread(target =  videoRecorder)
+recorder.start()
+
+tello.takeoff()
+tello.move_up(180)
+tello.rotate_counter_clockwise(360)
+tello.land()
+
+keepRecording=False
+recorder.join()
+
