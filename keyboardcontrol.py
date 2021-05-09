9c895dcb2f5446b659855edeb4bd930dc9021d1f
just_benedict_it <shsms9223@gmail.com>
2021-05-09 10:06:13 +0900

    tello_keycontrol[]

--- keyboardcontrol.py
+++ keyboardcontrol.py
@@ -15,13 +15,13 @@ def getKeyboardInput():
     elif kp.getKey('RIGHT') : lr = speed
 
     if kp.getKey('UP') : fb = speed
-    elif kp.getKey('RIGHT') : fb = -speed
+    elif kp.getKey('DOWN') : fb = -speed
 
     if kp.getKey('w') : ud = speed
     elif kp.getKey('s') : ud = -speed
 
-    if kp.getKey('a') : yv = speed
-    elif kp.getKey('d') : yv = -speed
+    if kp.getKey('a') : yv = -speed
+    elif kp.getKey('d') : yv = speed
 
     if kp.getKey("q") : me.land()
     if kp.getKey("e") : me.takeoff()
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from djitellopy import Tello
+from time import sleep
 
 tello = Tello()
 
 tello.connect()
 tello.takeoff()
 
-tello.move_left(100)
-tello.rotate_counter_clockwise(90)
-tello.move_forward(100)
-tello.move_backward(50)
-tello.rotate_clockwise(45)
-tello.move_right(30)
-
+# tello.move_left(30)
+# sleep(1)
+# tello.move_right(50)
+# sleep(1)
+# tello.rotate_counter_clockwise(90)
+# sleep(1)
+# tello.send_rc_control(-10,0,0,0)
+# sleep(2)
+#
+# tello.send_rc_control(0,20,0,0)
+# sleep(2)
+# tello.send_rc_control(0,-20,0,0)
+sleep(3)
 tello.land()
